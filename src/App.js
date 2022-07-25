c9107b0d32666783e1b91efb3b68af681dde6824
Evan Bond <43074752+EvanBond7@users.noreply.github.com>
2022-07-25 20:28:11 +0300

    set up a sticky topbar

--- src/App.js
+++ src/App.js
@@ -4,6 +4,56 @@ function App() {
   return (
     <div>
       <Topbar />
+      <div className='item'>My 1 text</div>
+      <div className='item'>My 2 text</div>
+      <div className='item'>My 3 text</div>
+      <div className='item'>My 4 text</div>
+      <div className='item'>My 5 text</div>
+      <div className='item'>My 6 text</div>
+      <div className='item'>My 7 text</div>
+      <div className='item'>My 8 text</div>
+      <div className='item'>My 9 text</div>
+      <div className='item'>My 10 text</div>
+      <div className='item'>My 11 text</div>
+      <div className='item'>My 12 text</div>
+      <div className='item'>My 13 text</div>
+      <div className='item'>My 14 text</div>
+      <div className='item'>My 15 text</div>
+      <div className='item'>My 16 text</div>
+      <div className='item'>My 17 text</div>
+      <div className='item'>My 18 text</div>
+      <div className='item'>My 19 text</div>
+      <div className='item'>My 20 text</div>
+      <div className='item'>My 21 text</div>
+      <div className='item'>My 22 text</div>
+      <div className='item'>My 23 text</div>
+      <div className='item'>My 24 text</div>
+      <div className='item'>My 25 text</div>
+      <div className='item'>My 26 text</div>
+      <div className='item'>My 27 text</div>
+      <div className='item'>My 28 text</div>
+      <div className='item'>My 29 text</div>
+      <div className='item'>My 30 text</div>
+      <div className='item'>My 31 text</div>
+      <div className='item'>My 32 text</div>
+      <div className='item'>My 33 text</div>
+      <div className='item'>My 34 text</div>
+      <div className='item'>My 35 text</div>
+      <div className='item'>My 36 text</div>
+      <div className='item'>My 37 text</div>
+      <div className='item'>My 38 text</div>
+      <div className='item'>My 39 text</div>
+      <div className='item'>My 40 text</div>
+      <div className='item'>My 41 text</div>
+      <div className='item'>My 42 text</div>
+      <div className='item'>My 43 text</div>
+      <div className='item'>My 44 text</div>
+      <div className='item'>My 45 text</div>
+      <div className='item'>My 46 text</div>
+      <div className='item'>My 47 text</div>
+      <div className='item'>My 48 text</div>
+      <div className='item'>My 49 text</div>
+      <div className='item'>My 50 text</div>
     </div>
   );
 }
