ab1df720855f5cb323d30a026cea184ea29fd730
Evan Bond <43074752+EvanBond7@users.noreply.github.com>
2022-07-29 19:50:10 +0300

    add a large widget component

--- src/components/WidgetLarge/WidgetLarge.jsx
+++ src/components/WidgetLarge/WidgetLarge.jsx
@@ -2,5 +2,81 @@ import React from 'react';
 import './widgetLarge.css';
 
 export default function WidgetLarge() {
-  return <div className='widget-lg'>Widget LARGE</div>;
+  const Button = ({ type }) => {
+    return <button className={'widgetLgButton ' + type}>{type}</button>;
+  };
+
+  return (
+    <div className='widget-lg'>
+      <h3 className='widget-lg-title'>Последние транзакции</h3>
+      <table className='widget-lg-table'>
+        <tr className='widget-lg-tr'>
+          <th className='widget-lg-th'>Покупатель</th>
+          <th className='widget-lg-th'>Дата</th>
+          <th className='widget-lg-th'>Сумма</th>
+          <th className='widget-lg-th'>Статус</th>
+        </tr>
+        <tr className='widget-lg-tr'>
+          <td className='widget-lg-user'>
+            <img
+              src='https://images.pexels.com/photos/4172933/pexels-photo-4172933.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+              alt=''
+              className='widget-lg-img'
+            />
+            <span className='widget-lg-name'>Евгений Зимин</span>
+          </td>
+          <td className='widget-lg-date'>2 Мая 2022</td>
+          <td className='widget-lg-amount'>5000&#x20bd;</td>
+          <td className='widget-lg-status'>
+            <Button type='Approved' />
+          </td>
+        </tr>
+        <tr className='widget-lg-tr'>
+          <td className='widget-lg-user'>
+            <img
+              src='https://images.pexels.com/photos/4172933/pexels-photo-4172933.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+              alt=''
+              className='widget-lg-img'
+            />
+            <span className='widget-lg-name'>Евгений Зимин</span>
+          </td>
+          <td className='widget-lg-date'>2 Мая 2022</td>
+          <td className='widget-lg-amount'>5000&#x20bd;</td>
+          <td className='widget-lg-status'>
+            <Button type='Approved' />
+          </td>
+        </tr>
+        <tr className='widget-lg-tr'>
+          <td className='widget-lg-user'>
+            <img
+              src='https://images.pexels.com/photos/4172933/pexels-photo-4172933.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+              alt=''
+              className='widget-lg-img'
+            />
+            <span className='widget-lg-name'>Евгений Зимин</span>
+          </td>
+          <td className='widget-lg-date'>2 Мая 2022</td>
+          <td className='widget-lg-amount'>5000&#x20bd;</td>
+          <td className='widget-lg-status'>
+            <Button type='Approved' />
+          </td>
+        </tr>
+        <tr className='widget-lg-tr'>
+          <td className='widget-lg-user'>
+            <img
+              src='https://images.pexels.com/photos/4172933/pexels-photo-4172933.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+              alt=''
+              className='widget-lg-img'
+            />
+            <span className='widget-lg-name'>Евгений Зимин</span>
+          </td>
+          <td className='widget-lg-date'>2 Мая 2022</td>
+          <td className='widget-lg-amount'>5000&#x20bd;</td>
+          <td className='widget-lg-status'>
+            <Button type='Approved' />
+          </td>
+        </tr>
+      </table>
+    </div>
+  );
 }
