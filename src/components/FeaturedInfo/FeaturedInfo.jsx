320bd45138f1143d39e518eb2ed1f0376357db99
Evan Bond <43074752+EvanBond7@users.noreply.github.com>
2022-07-29 13:06:21 +0300

    fix color of featured-icon

--- src/components/FeaturedInfo/FeaturedInfo.jsx
+++ src/components/FeaturedInfo/FeaturedInfo.jsx
@@ -6,11 +6,11 @@ export default function FeaturedInfo() {
   return (
     <div className='featured'>
       <div className='featured-item'>
-        <span className='featured-title'>Доход</span>
+        <span className='featured-title'>Доходы</span>
         <div className='featured-money-container'>
           <span className='featured-money'>2,520&#x20bd;</span>
           <span className='featured-money-rate'>
-            -12.7 <ArrowDownward className='featured-icon negavite' />
+            -12.7 <ArrowDownward className='featured-icon negative' />
           </span>
         </div>
         <span className='featured-sub'>Сравнить с последним месяцем</span>
@@ -20,7 +20,7 @@ export default function FeaturedInfo() {
         <div className='featured-money-container'>
           <span className='featured-money'>5,200&#x20bd;</span>
           <span className='featured-money-rate'>
-            -3.5 <ArrowDownward className='featured-icon negavite' />
+            -3.5 <ArrowDownward className='featured-icon negative' />
           </span>
         </div>
         <span className='featured-sub'>Сравнить с последним месяцем</span>
