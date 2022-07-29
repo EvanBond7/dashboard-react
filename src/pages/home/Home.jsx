7c183f595305ac7e292af96a08bea341f16dfecf
Evan Bond <43074752+EvanBond7@users.noreply.github.com>
2022-07-29 14:00:52 +0300

    Reusable Chart Component for dashboard

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
+import Chart from '../../components/Chart/Chart';
 import FeaturedInfo from '../../components/FeaturedInfo/FeaturedInfo';
 import './home.css';
+import { userData } from '../../db.js';
 
 export default function Home() {
   return (
     <div className='home'>
       <FeaturedInfo />
+      <Chart
+        data={userData}
+        title='Активность пользователей'
+        dataKey='Активные пользователи'
+        grid
+      />
     </div>
   );
 }
