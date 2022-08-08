a054df2dcac289b8848214599458d4556b5ce4a9
Evan Bond <43074752+EvanBond7@users.noreply.github.com>
2022-08-08 14:04:12 +0300

    add updated Chart Component

--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -9,14 +9,21 @@ import {
 } from 'recharts';
 import './chart.css';
 
-export default function Chart({ title, data, dataKey, grid }) {
+export default function Chart({
+  title,
+  data,
+  dataKey,
+  grid,
+  colorX,
+  colorLine,
+}) {
   return (
     <div className='chart'>
       <h3 className='chart-title'>{title}</h3>
       <ResponsiveContainer width='100%' aspect={4 / 1}>
         <LineChart data={data}>
-          <XAxis dataKey='name' stroke='#5550bd' />
-          <Line type='monotone' dataKey={dataKey} stroke='#5550bd' />
+          <XAxis dataKey='name' stroke={colorX} />
+          <Line type='monotone' dataKey={dataKey} stroke={colorLine} />
           <Tooltip />
           {grid && <CartesianGrid stroke='#e0dfdf' strokeDasharray='5 5' />}
         </LineChart>
